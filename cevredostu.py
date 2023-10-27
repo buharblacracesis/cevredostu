21b7417bb6e04339c9a79f3fa9c95d0f60f30258
buharblacracesis <145800000+buharblacracesis@users.noreply.github.com>
2023-10-27 21:25:23 +0300

    cevredostu.py

--- cevredostu.py
+++ cevredostu.py
@@ -22,7 +22,7 @@ async def heh(ctx, count_heh = 5):
 async def seskirliliğinedir(ctx):
     await ctx.send(f'Merhaba {bot.user}! Gürültü kirliliği veya diğer adıyla ses kirliliği, insan veya hayvan yaşamını olumsuz etkileyen, dengesini bozan her türlü insan, hayvan ya da makine kaynaklı ses oluşumudur. Gürültü kirliliğinin en yaygın biçimlerinden biri, özellikle motorlu araçların neden olduğu kirliliktir.')
 
-bot.run("MTE2NzUxMDg1ODExNzY4MTI3NA.G2chEO.uukJ1521BOQxc4Awt2km44ocWEHtTxYNS-PsaA")
+bot.run("token is hereee")
 
 
 
